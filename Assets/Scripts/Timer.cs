dfca2e11170cd1cd466aafb317652fa8cf04104b
Daniel C <dcdolphy@gmail.com>
2017-07-17 22:50:29 +0100

    Timer now resets upon restarting the game.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,22 +4,29 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
 
     // Variables
-    Text timer;
+    private Text timer;
     private float start;
+    private float t;
+    private string minutes;
+    private string seconds;
 
     void Start()
     {
         timer = GetComponent<Text>();
         start = Time.time;
+        t = 0;
+        start = 0;
+        minutes = null;
+        seconds = null;
     }
-	
+
 	// Update is called once per frame
 	void Update ()
     {
-        float t = start += Time.deltaTime;
+        t = start += Time.deltaTime;
 
-        string minutes = ((int)t / 60).ToString();
-        string seconds = (t % 60).ToString("f2");
+        minutes = ((int)t / 60).ToString();
+        seconds = (t % 60).ToString("f2");
 
         timer.text = "Time: " + minutes + ":" + seconds;
 	}
